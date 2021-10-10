4376df290626d5556367d529248aec634bb5dff8
Sergio Lamar <sergio.lamar@gmail.com>
2021-10-09 18:52:59 -0700

    final generator

--- script.js
+++ script.js
@@ -9,61 +9,77 @@ var special = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '?'];
 var allCombos = [];
 
 // Initiate prompts to determine password criteria. 
-function generatePassword () {  
+function generatePassword () { 
+  // First, determine the length variable - prompt input
   var length = prompt("Choose a password length between 8 & 128"); 
     while ((length < 8) || (length > 128)) {
-      alert("Password mus be between 8 and 128 characters!");
+     alert("Password must be between 8 and 128 characters!");
      length = prompt("Choose a password length between 8 & 128"); 
     }
+  
+  // Second, determine remaining parameters for password criteria - confirmation window
   var userNumbers = confirm("Would you like to include numbers in you password?");
-  var userLowerCase = confirm("Would you like to include lower case letters in your password");
+  var userLowerCase = confirm("Would you like to include lower case letters in your password?");
   var userUpperCase = confirm ("Would you like to include upper case letters in your password?");
-  var userSpecial = confirm ("Would you like to include special characters in your password");
+  var userSpecial = confirm ("Would you like to include special characters in your password?");
+   // Condition that requires at least one selection is made, if not loops confirmation questions.  
    while (userNumbers, userLowerCase, userUpperCase, userSpecial === false) {
      alert("You must choose at least one!");
      userNumbers = confirm("Would you like to include numbers in you password?");
-     userLowerCase = confirm("Would you like to include lower case letters in your password");
+     userLowerCase = confirm("Would you like to include lower case letters in your password?");
      userUpperCase = confirm ("Would you like to include upper case letters in your password?");
-    userSpecial = confirm ("Would you like to include special characters in your password");
+     userSpecial = confirm ("Would you like to include special characters in your password?");
    }
 
   // declare password varibale to hold char for user password
-  var userNumbers = [];
-  // track number of choices
-  var numOfChoices = 0;
-  // check for each
-  
+  var allCombos = [];
+  var finalPassword = "";
 
   // case 1: numbers
-  if(useNumbers)  {
-      allCombos = allCombos.concat(numbers)
-      password.push(numbers[Math.floor(Math.random()*numbers.length)])
-      numOfChoices++
-
-
+  if (userNumbers)  {
+    for (var i of numbers)
+      allCombos.push(i);
   }
 
-  // case 2
+  // case 2: lowerCase
+  if (userLowerCase)  {
+    for (var i of lowerCase)
+      allCombos.push(i);
+  }
 
+  // case 3: upperCase
+  if (userUpperCase)  {
+    for (var i of upperCase)
+      allCombos.push(i);
+  }
 
-  // case 3
+  // case 4: special
+  if (userSpecial)  {
+    for (var i of special)
+      allCombos.push(i);
+  }
 
-  // case4
+  // log allCombos to console
+  console.log(allCombos);
 
-
-  // loop through the remanining number of characters for the password araray; 
-
-
-  return password; // might use join
+  // math to create the password
+    for (var i = 0; i < length; i++) {
+      finalPassword += allCombos[Math.floor(Math.random() * allCombos.length)];
+  }
   
-}
+  // log final password to the console
+  console.log(finalPassword);
+
+  // print final password to console
+  return finalPassword;
+  }
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var finalPassword = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = finalPassword;
 }
 
 // Add event listener to generate button
